0d3084e3eecf52c92155552d40717e8184b9d07c
Mahyar <mahyarmalek58@gmail.com>
2026-06-24 18:15:16 +0330

    prototype build

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Admin:
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def add_staff(self):
+        pass
+
+
+
+
+
+class staff:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
