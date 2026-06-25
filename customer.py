10819287d411ea2600ae3c400994ff0fb1d22df2
ali soltani <alisoltani2210@gmail.com>
2026-06-25 20:36:35 +0330

    Create Customer.py
    first implemention of customer class and validations.

---
+++ customer.py
@@ -0,0 +1,76 @@
+import re
+class Customer:
+    email_pattern = re.compile(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+    password_pattern = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@&]).+$')
+    customer_list = []
+    def __init__(self, username, email, password):
+        self.__username = username
+        self.email = email
+        self.password = password
+        self.my_cards = []
+
+    @property
+    def username(self):
+        return self.__username
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        if not Customer.email_pattern.fullmatch(value):
+            raise ValueError("Invalid email address")
+        self.__email = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if not Customer.password_pattern.fullmatch(value):
+            raise ValueError("Password must contain letter, digit and one of @ or &")
+        self.__password = value
+
+    def add_customer(self):
+        if Customer.find_username(self.username) is not None:
+            return False
+        if Customer.find_email(self.email) is not None:
+            return False
+        Customer.customer_list.append(self)
+        return True
+
+    def add_card(self,card):
+        found_card = self.find_card(card.card_id)
+        if found_card is not None:
+            return False
+        self.my_cards.append(card)
+        return True
+
+
+    def find_card(self, card_id):
+        for card in self.my_cards:
+            if card.card_id == card_id:
+                return card
+        return None
+    @classmethod
+    def find_username(cls,username):
+        for customer in cls.customer_list:
+            if customer.username == username:
+                return customer
+        return None
+
+    @classmethod
+    def find_email(cls,email):
+        for customer in cls.customer_list:
+            if customer.email == email:
+                return customer
+        return None
+
+    def remove_card(self, card):
+        found_card = self.find_card(card.card_id)
+        if found_card is None:
+            return False
+        self.my_cards.remove(found_card)
+        return True
