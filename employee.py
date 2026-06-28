15840505a3cbf91eb0da1fb5355c04f8134eb92b
Sam Ahmadi <samahmadi13851385@gmail.com>
2026-06-28 21:53:27 +0330

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,49 @@
+import re
+
+class Employee:
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+employees_list = [Employee("karmand1", "Kd@1", "reza", "sam@email.com")]
+lines_list = []
+trains_list = []
+
+def is_valid_password(password):
+    pattern = r'^(?=.*[a-zA-Z])(?=.*\d)(?=.*[&@])[a-zA-Z0-9&@]+$'
+    temp = re.match(pattern, password)
+    if temp:
+        return True
+    else:
+        return False
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$'
+    temp = re.match(pattern, email)
+    if temp :
+        return True
+    else:
+        return False
+
+
+def check_username(username, target_list):
+    for item in target_list:
+        if item.username == username:
+            return True
+    return False
+
+
+def check_email(email, target_list):
+    for item in target_list:
+        if item.email == email:
+            return True
+        return False
+
+
+
+
+
+
+
